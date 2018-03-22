61dd6734c8319b41af0a5a04cdda85e25c605390
lifedotgetrekt <37666590+lifedotgetrekt@users.noreply.github.com>
2018-03-22 13:01:09 -0400

    Add files via upload

---
+++ GameTest.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class GameTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameTest
+{
+    public static void main(String args[]){
+        Grid g = new Grid(10);
+        Robot aChar = new Robot(0,0);
+        
+        
+        g.addCharacter(aChar);
+        System.out.print(g.display());
+    }
+}
