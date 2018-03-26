0cb1672ad18e26e7eae991b60a07b949ef5d8de0
lifedotgetrekt <37666590+lifedotgetrekt@users.noreply.github.com>
2018-03-26 12:10:27 -0400

    Update GameTest.java

--- GameTest.java
+++ GameTest.java
@@ -9,7 +9,7 @@ public class GameTest
 {
     public static void main(String args[]){
         Grid g = new Grid(10);
-        Robot aChar = new Robot(0,0);
+        Robot aChar = new Robot(0,0,"Bender",10);
         
         
         g.addCharacter(aChar);
