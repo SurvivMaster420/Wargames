8db396b2615fb4ecbfd7ec70f596387272594280
SurvivMaster420 <37666594+SurvivMaster420@users.noreply.github.com>
2018-03-26 11:49:58 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -5,24 +5,26 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Hero extends Character
+public class Hero extends Character //extends character so it can share methods with robot class
 {
-  private int x;
+  private int x; //private necessary so it differs from other characters
   private int y;
-  int hp;
-  int ammo;
-  public Hero(int newX, int newY){
+  int hp; //amount of health of hero
+  int ammo; //amount of bullets hero can shoot
+  
+  public Hero(int newX, int newY){ //initializes variables for hero. Gives specified amount of hp and ammo
       super(newX, newY);
       hp = 5;
       ammo = 8;
+    
   }
-  public int getX(){
+  public int getX(){ //return row number that hero is in
     return x;
     }
-  public int getY(){
+  public int getY(){ //return column number that hero is in
     return y;
     }
-  public void attack(Character aChar){
+  public void attack(Character aChar){ //shoots a bullet towards specified target
       
   }
 }
