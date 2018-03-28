9dd1eca703c174d8f2b7c8a3c80aecb1f48f5548
SurvivMaster420 <37666594+SurvivMaster420@users.noreply.github.com>
2018-03-28 12:17:26 -0400

    Update Healthpack.java

--- Healthpack.java
+++ Healthpack.java
@@ -5,21 +5,21 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Healthpack extends Character
+public class Healthpack extends Character //extends character so it can interact with hero and robot class
 {
    private int x;
    private int y;
    public Healthpack(int newX, int newY, String newName, int newHp){
-       super(newX, newY, newName, newHp, newAmmo);
+       super(newX, newY, newName, newHp, newAmmo); //initializes x and y values of health pack
    }
-   public int getX(){
+   public int getX(){ //returns column position
     return x;
     }
-   public int getY(){
+   public int getY(){ //returns row position
     return y;
     }
    public void healChar(Character aChar, Grid grid){
-       aChar.hp++;
+       aChar.hp++; //heals character for one health point when touched
        
    }
 }
