d371dfaf507af7e1250eb4b18824ab4abeda80b1
SurvivMaster420 <37666594+SurvivMaster420@users.noreply.github.com>
2018-03-28 12:23:34 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -5,9 +5,9 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Hero extends Character
+public class Hero extends Character //all methods are taken from Character
 {
   public Hero(int newX, int newY, String newName, int newHp, int newAmmo){
-      super(newX, newY, newName, newHp, newAmmo);
+      super(newX, newY, newName, newHp, newAmmo); //constructor inherits all from character
   }
 }
