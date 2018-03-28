175c4cac8eebdc3c89f3df236c46234df48a3a81
SurvivMaster420 <37666594+SurvivMaster420@users.noreply.github.com>
2018-03-28 12:16:15 -0400

    Update Ammopack.java

--- Ammopack.java
+++ Ammopack.java
@@ -5,22 +5,22 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Ammopack extends Character
+public class Ammopack extends Character //Extends character so it can interact with hero and robot
 {
    private int x;
    private int y;
    public Ammopack(int newX, int newY, String newName, int newHp){
-       super(newX, newY, newName, newHp);
-       int ammo = 1;
+       super(newX, newY, newName, newHp); //initializes position to put ammo pack in
+       int ammo = 1; 
    }
-   public int getX(){
+   public int getX(){ //returns column position
     return x;
     }
-   public int getY(){
+   public int getY(){ //returns row position
     return y;
     }
    public void refillAmmo(Character aChar, Grid grid){
-       aChar.ammo++;
+       aChar.ammo++; //adds 1 ammo to a character that touches the ammo pack
        
    }
 }
