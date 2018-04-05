4f823bbb085463847ec142129160b4281fc70406
lifedotgetrekt <37666590+lifedotgetrekt@users.noreply.github.com>
2018-04-05 17:51:06 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -8,6 +8,18 @@
 public class Hero extends Character //all methods are taken from Character
 {
   public Hero(int newX, int newY, String newName, int newHp, int newAmmo){
-      super(newX, newY, newName, newHp, newAmmo); //constructor inherits all from character
+     super(newX, newY, newName, newHp, newAmmo); //constructor inherits all from character
+  }
+  public String getGridIcon(){
+     return "\u25A3";//black square in white square
+  }
+  public String toString(){
+     String out = "";
+     out += "Name: " + getName() + " " + getGridIcon()+"\n";
+     out += "Class: Hero\n";
+     out += "HP: " + getHp() + "\n";
+     out += "Ammo: " + getAmmo() + "\n";
+     out += "Coordinates: ("+getX()+", "+getY()+")\n";
+     return out;
   }
 }
