ab992a1cc7109fa1025507cb3fb666b20cb59454
lifedotgetrekt <37666590+lifedotgetrekt@users.noreply.github.com>
2018-04-05 17:50:25 -0400

    Update Healthpack.java

--- Healthpack.java
+++ Healthpack.java
@@ -9,17 +9,21 @@ public class Healthpack extends Character //extends character so it can interact
 {
    private int x;
    private int y;
-   public Healthpack(int newX, int newY, String newName, int newHp){
+   private boolean isOnBoard;
+   public Healthpack(int newX, int newY, String newName, int newHp, int newAmmo){
        super(newX, newY, newName, newHp, newAmmo); //initializes x and y values of health pack
    }
-   public int getX(){ //returns column position
-    return x;
-    }
-   public int getY(){ //returns row position
-    return y;
-    }
    public void healChar(Character aChar, Grid grid){
-       aChar.hp++; //heals character for one health point when touched
-       
+       aChar.heal();//heals character for one health point when touched
+       grid.setSpot(this.getX(), this.getY(), -1);
+   }
+   public String getGridIcon(){
+       return "\u25A5";//vertical-striped box
+   }
+   public String toString(){
+       String out = "";
+       out += "A Healthpack " + getGridIcon() + "\n";
+       out += "Coordinates: ("+getX()+", "+getY()+")\n";
+       return out;
    }
 }
